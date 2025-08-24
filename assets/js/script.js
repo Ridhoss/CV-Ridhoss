7800b93e33606bf9990186cbe379b7884f4f1fa4
Ridho Sulistyo <ridhosulistyo1314@gmail.com>
2025-08-24 18:06:12 +0700

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,204 @@
+import { animate, inView } from "https://cdn.jsdelivr.net/npm/motion@latest/+esm"
+
+// sidebar action
+const sidebar = document.getElementById('sidebar');
+const btnOpen = document.getElementById('btn-open-sidebar');
+const btnClose = document.getElementById('btn-close-sidebar');
+
+sidebar.style.transform = "translateX(100%)"
+sidebar.style.opacity = 0
+
+btnOpen.addEventListener("click", () => {
+  animate(btnOpen, { rotate: 360 })
+  animate(btnClose, { rotate: 360 })
+  animate(sidebar, { x: ["100%", 0], opacity: [0, 1] }, { duration: 0.5 });
+})
+
+btnClose.addEventListener("click", () => {
+  animate(btnOpen, { rotate: -360 })
+  animate(btnClose, { rotate: -360 })
+  animate(sidebar, { x: [0, "100%"], opacity: [1, 0] }, { duration: 0.5 });
+})
+// sidebar action end
+
+
+// home
+inView(".home-text", (element) => {
+  animate(element, { opacity: 1, y: [-10, 0] },{ duration: 1})
+
+  return () => animate(element, { opacity: 0, y: -10 })
+})
+// home end
+
+// about
+inView(".about-text-1", (element) => {
+  animate(element, { opacity: 1, y: [-10, 0] },{ duration: 1})
+
+  return () => animate(element, { opacity: 0, y: -10 })
+})
+
+inView(".about-text-2", (element) => {
+  animate(element, { opacity: 1, x: [-10, 0] },{ duration: 1})
+
+  return () => animate(element, { opacity: 0, x: -10 })
+})
+
+inView(".about-social-media", (element) => {
+  animate(element, { opacity: 1, x: [-10, 0] },{ duration: 1})
+
+  return () => animate(element, { opacity: 0, x: -10 })
+})
+
+inView(".about-image", (element) => {
+  animate(element, { opacity: 1, x: [10, 0] },{ duration: 1})
+
+  return () => animate(element, { opacity: 0, x: 10 })
+})
+// about end
+
+// education
+inView(".title-education", (element) => {
+  animate(element, { opacity: 1, y: [-10, 0] },{ duration: 1})
+
+  return () => animate(element, { opacity: 0, y: -10 })
+})
+inView(".card-education-1", (element) => {
+  animate(element, { opacity: 1, x: [-10, 0] },{ duration: 1})
+
+  return () => animate(element, { opacity: 0, x: -10 })
+})
+inView(".card-education-2", (element) => {
+  animate(element, { opacity: 1, x: [10, 0] },{ duration: 1})
+
+  return () => animate(element, { opacity: 0, x: 10 })
+})
+// education end
+
+// education
+inView(".title-experience", (element) => {
+  animate(element, { opacity: 1, x: [10, 0] },{ duration: 1})
+
+  return () => animate(element, { opacity: 0, x: 10 })
+})
+inView(".card-experience", (element) => {
+  animate(element, { opacity: 1, x: [-10, 0] },{ duration: 1})
+
+  return () => animate(element, { opacity: 0, x: -10 })
+})
+inView(".card-experience-1", (element) => {
+  animate(element, { opacity: 1, y: [10, 0] },{ duration: 1})
+
+  return () => animate(element, { opacity: 0, y: 10 })
+})
+inView(".card-experience-2", (element) => {
+  animate(element, { opacity: 1, y: [10, 0] },{ duration: 1})
+
+  return () => animate(element, { opacity: 0, y: 10 })
+})
+// education end
+
+
+// skilss
+inView(".skills-title", (element) => {
+  animate(element, { opacity: 1, y: [-10, 0] },{ duration: 1})
+
+  return () => animate(element, { opacity: 0, y: -10 })
+})
+inView(".skills-card-language", (element) => {
+  animate(element, { opacity: 1, y: [10, 0] },{ duration: 1})
+
+  return () => animate(element, { opacity: 0, y: 10 })
+})
+inView(".skills-card", (element) => {
+  animate(element, { opacity: 1, x: [10, 0] },{ duration: 1})
+
+  return () => animate(element, { opacity: 0, x: 10 })
+})
+
+inView(".skills-1", (element) => {
+  element.style.width = "0%"
+  animate(element, 
+      { width: ["0%", "60%"] }, 
+      { duration: 1.5, easing: "ease-out" }
+  )
+
+  return () => animate(element, { width: ["0%", "60%"] })
+})
+inView(".skills-2", (element) => {
+  element.style.width = "0%"
+  animate(element, 
+      { width: ["0%", "80%"] }, 
+      { duration: 1.5, easing: "ease-out" }
+  )
+
+  return () => animate(element, { width: ["0%", "80%"] })
+})
+inView(".skills-3", (element) => {
+  element.style.width = "0%"
+  animate(element, 
+      { width: ["0%", "80%"] }, 
+      { duration: 1.5, easing: "ease-out" }
+  )
+
+  return () => animate(element, { width: ["0%", "80%"] })
+})
+inView(".skills-4", (element) => {
+  element.style.width = "0%"
+  animate(element, 
+      { width: ["0%", "70%"] }, 
+      { duration: 1.5, easing: "ease-out" }
+  )
+
+  return () => animate(element, { width: ["0%", "70%"] })
+})
+inView(".skills-5", (element) => {
+  element.style.width = "0%"
+  animate(element, 
+      { width: ["0%", "90%"] }, 
+      { duration: 1.5, easing: "ease-out" }
+  )
+
+  return () => animate(element, { width: ["0%", "90%"] })
+})
+inView(".skills-6", (element) => {
+  element.style.width = "0%"
+  animate(element, 
+      { width: ["0%", "75%"] }, 
+      { duration: 1.5, easing: "ease-out" }
+  )
+
+  return () => animate(element, { width: ["0%", "75%"] })
+})
+// slills end
+
+// portfolio
+inView(".portfolio-title", (element) => {
+  animate(element, { opacity: 1, x: [-10, 0] },{ duration: 1})
+
+  return () => animate(element, { opacity: 0, x: -10 })
+})
+inView(".portfolio-slider", (element) => {
+  animate(element, { opacity: 1, y: [10, 0] },{ duration: 1})
+
+  return () => animate(element, { opacity: 0, y: 10 })
+})
+// portfolio end
+
+// gallery
+inView(".gallery-title", (element) => {
+  animate(element, { opacity: 1, y: [-10, 0] },{ duration: 1})
+
+  return () => animate(element, { opacity: 0, y: -10 })
+})
+inView(".gallery-photo", (element) => {
+  animate(element, { opacity: 1, y: [-10, 0] },{ duration: 1})
+
+  return () => animate(element, { opacity: 0, y: -10 })
+})
+// gallery end
+
+inView(".footer", (element) => {
+  animate(element, { opacity: 1, y: [10, 0] },{ duration: 1})
+
+  return () => animate(element, { opacity: 0, y: 10 })
+})
